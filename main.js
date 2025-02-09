ab32977097ef985ae470ea97b8910bb5f1f9f4cf
Arthur Pimenta <arthurstt22@gmail.com>
2025-02-09 16:05:52 -0300

    Criando as condições de verificação

--- main.js
+++ main.js
@@ -6,12 +6,16 @@ document
 function adicionarNota() {
   const notaAluno = document.getElementById("nota").value;
   const notaFormat = Number(notaAluno);
-  console.log(notaFormat);
+
+  if (notaAluno === "") {
+    alert("Digite uma nota");
+    return;
+  }
 
   if (!isNaN(notaFormat) && notaFormat >= 0 && notaFormat <= 10) {
     const lista = document.getElementById("listaNotas");
     const item = document.createElement("li");
-    item.textContent = notaFormat;
+    item.textContent = notaFormat.toFixed(2);
     lista.appendChild(item);
     document.getElementById("nota").value = "";
   } else {
@@ -24,11 +28,16 @@ function calcularMedia() {
   soma = 0;
   media = 0;
 
+  if (notas.length === 0) {
+    alert("Adicione notas para calcular a média");
+    return;
+  }
+
   for (let i = 0; i < notas.length; i++) {
     soma += Number(notas[i].textContent);
   }
 
   media = soma / notas.length;
   const resultado = document.getElementById("resultado");
-  resultado.textContent = `A média é: ${media}`;
+  resultado.textContent = `A média é: ${media.toFixed(2)}`;
 }
