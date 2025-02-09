6d43a56518e40280c4bfabed8383873fae39c048
Arthur Pimenta <arthurstt22@gmail.com>
2025-02-09 15:22:09 -0300

    testando a branch e finalizando o projeto

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
 document.getElementById("inNota").addEventListener("click", adicionarNota);
+document
+  .getElementById("calcularMedia")
+  .addEventListener("click", calcularMedia);
 
 function adicionarNota() {
   const notaAluno = document.getElementById("nota").value;
   const notaFormat = Number(notaAluno);
   console.log(notaFormat);
+
+  if (!isNaN(notaFormat) && notaFormat >= 0 && notaFormat <= 10) {
+    const lista = document.getElementById("listaNotas");
+    const item = document.createElement("li");
+    item.textContent = notaFormat;
+    lista.appendChild(item);
+    document.getElementById("nota").value = "";
+  } else {
+    alert("Digite um número entre 0 e 10");
+  }
+}
+
+function calcularMedia() {
+  const notas = document.querySelectorAll("#listaNotas li");
+  soma = 0;
+  media = 0;
+
+  for (let i = 0; i < notas.length; i++) {
+    soma += Number(notas[i].textContent);
+  }
+
+  media = soma / notas.length;
+  const resultado = document.getElementById("resultado");
+  resultado.textContent = `A média é: ${media}`;
 }
